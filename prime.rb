72192346f6c2dda4e27897bfc76d397baf42d6d2
Tony DiNitto <think41c@gmail.com>
2015-03-27 00:28:50 -0600

    More logic to determine primes implemented.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,17 @@
 class Prime
 
   def self.nth(x)
+    primes = []
     if x == 0 
       raise ArgumentError
     end
-    2
     
+    until primes.length == x 
+      primes.last
+      primes << 0 # Find a new prime number
+    end
+
+    2
   end
 
 end
