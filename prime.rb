a14cd0b141b77b413ed063904aed7b15bbd8e9b3
Tony DiNitto <think41c@gmail.com>
2015-03-27 00:37:57 -0600

    Separation of methods for find a prime number.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,26 @@
 class Prime
 
   def self.nth(x)
+    starting_numb = 1
     primes = []
+
     if x == 0 
       raise ArgumentError
     end
     
-    until primes.length == x 
+    until primes.length == x
+      self.determine_if_prime
+      # start_numb % (A range of numbers up to start_numb / 2) == 0  
       primes << 2 # Find a new prime number
+      starting_numb += 1 
     end
 
     primes.last
   end
 
+  def self.determine_if_prime
+    puts "hi"
+  end
 end
+
+p Prime.nth(2)
